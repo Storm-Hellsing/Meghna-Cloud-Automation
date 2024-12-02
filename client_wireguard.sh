49b15d8b3b8edfa475c06fb6dbdea5042e45d040
Storm-Hellsing <sg.abid@hotmail.com>
2024-12-02 22:37:10 +0600

    Wiregurard: Server and Cilent Configurations Updated (Unstable)

---
+++ client_wireguard.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+echo "
+┓ ┏  ┓          ┏┳┓                             
+┃┃┃┏┓┃┏┏┓┏┳┓┏┓   ┃ ┏┓                           
+┗┻┛┗ ┗┗┗┛┛┗┗┗    ┻ ┗┛                           
+┓ ┏•             ┓  ┏┓┓•       ┳      ┓┓   •    
+┃┃┃┓┏┓┏┓┏┓┓┏┏┓┏┓┏┫  ┃ ┃┓┏┓┏┓╋  ┃┏┓┏╋┏┓┃┃┏┓╋┓┏┓┏┓
+┗┻┛┗┛ ┗ ┗┫┗┻┗┻┛ ┗┻  ┗┛┗┗┗ ┛┗┗  ┻┛┗┛┗┗┻┗┗┗┻┗┗┗┛┛┗
+         ┛                                      
+"
+
+if [ $(id -u) -ne 0 ]; then 
+    echo "Permission Denied: Root Privileges Required"
+else 
+    if [ ! -f /etc/wireguard/wg0.conf ]; then
+        echo "Server configuration not found. Please configure the server first."
+    else
+        SERVER_PUBLIC_IP=$(curl -s ifconfig.me)
+        read -p "Is this the correct public IP of the server - $SERVER_PUBLIC_IP? [Y/N]: " CONFIRM_IP
+        
+        if [ $CONFIRM_IP == "y" ] || [ $CONFIRM_IP == "Y" ]; then
+            SERVER_IP_BLOCK=$(grep "Address" /etc/wireguard/wg0.conf | awk '{print $3}')
+            SERVER_PRIVATE_IP=$(echo $SERVER_IP_BLOCK | cut -d '/' -f1)
+            SERVER_SUBNET=$(echo $SERVER_IP_BLOCK | cut -d '/' -f2)
+            SERVER_LISTENING_PORT=$(grep "ListenPort" /etc/wireguard/wg0.conf | awk '{print $3}')
+
+            TOTAL_IPS=$((2**(32-SERVER_SUBNET)))
+            AVAILABLE_IPS=$((TOTAL_IPS - 2))
+
+            echo ""
+            echo "The server is configured with the private IP block: $SERVER_IP_BLOCK"
+            echo "You can create up to $AVAILABLE_IPS clients."
+            echo ""
+
+            if [ ! -d /etc/wireguard/clients ]; then
+                mkdir /etc/wireguard/clients
+            fi
+
+            while true; do
+                read -p "How many clients would you like to create? " NUM_CLIENTS
+                if [[ $NUM_CLIENTS =~ ^[0-9]+$ ]] && [ $NUM_CLIENTS -le $AVAILABLE_IPS ] && [ $NUM_CLIENTS -ge 1 ]; then
+                    break
+                else
+                    echo "Invalid number of clients. Please enter a number between 1 and $AVAILABLE_IPS."
+                fi
+            done
+
+            IFS='.' read -r -a OCTETS <<< "$SERVER_PRIVATE_IP"
+
+            for ((i=1; i<=NUM_CLIENTS; i++)); do
+                umask 077
+                wg genkey > /etc/wireguard/clients/client${i}_privatekey
+                wg pubkey < /etc/wireguard/clients/client${i}_privatekey > /etc/wireguard/clients/client${i}_publickey
+                wg genpsk > /etc/wireguard/clients/client${i}_presharedkey
+
+                CLIENT_PRIVATE_KEY=$(</etc/wireguard/clients/client${i}_privatekey)
+                CLIENT_PUBLIC_KEY=$(</etc/wireguard/clients/client${i}_publickey)
+                CLIENT_PRESHARED_KEY=$(</etc/wireguard/clients/client${i}_presharedkey)
+                DNS_SERVER="8.8.8.8, 8.8.4.4, 1.1.1.1"
+
+                OCTETS[3]=$((OCTETS[3] + 1))
+                for ((j=3; j>=0; j--)); do
+                    if [ ${OCTETS[j]} -gt 254 ]; then
+                        OCTETS[j]=1
+                        if [ $j -gt 0 ]; then
+                            OCTETS[$((j-1))]=$((OCTETS[$((j-1))] + 1))
+                        fi
+                    fi
+                done
+
+                CLIENT_IP="${OCTETS[0]}.${OCTETS[1]}.${OCTETS[2]}.${OCTETS[3]}"
+                while [[ "$CLIENT_IP" == "$SERVER_PRIVATE_IP" || "$CLIENT_IP" == *".0" || "$CLIENT_IP" == *".255" ]]; do
+                    OCTETS[3]=$((OCTETS[3] + 1))
+                    for ((j=3; j>=0; j--)); do
+                        if [ ${OCTETS[j]} -gt 254 ]; then
+                            OCTETS[j]=1
+                            if [ $j -gt 0 ]; then
+                                OCTETS[$((j-1))]=$((OCTETS[$((j-1))] + 1))
+                            fi
+                        fi
+                    done
+                    CLIENT_IP="${OCTETS[0]}.${OCTETS[1]}.${OCTETS[2]}.${OCTETS[3]}"
+                done
+
+                CLIENT_CONFIG="/etc/wireguard/clients/client${i}.conf"
+                cat << EOF >$CLIENT_CONFIG
+[Interface]
+PrivateKey = $CLIENT_PRIVATE_KEY
+Address = $CLIENT_IP/32
+DNS = $DNS_SERVER
+
+[Peer]
+PublicKey = $(grep "PrivateKey" /etc/wireguard/wg0.conf | awk '{print $3}')
+PresharedKey = $CLIENT_PRESHARED_KEY
+Endpoint = $SERVER_PUBLIC_IP:$SERVER_LISTENING_PORT
+AllowedIPs = 0.0.0.0/0
+PersistentKeepalive = 25
+EOF
+
+                echo "Client $i configuration created with IP $CLIENT_IP"
+            done
+        else
+            echo "Public IP confirmation failed."
+        fi
+    fi
+fi
---
+++ server_wireguard.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+echo "
+┓ ┏  ┓          ┏┳┓                                
+┃┃┃┏┓┃┏┏┓┏┳┓┏┓   ┃ ┏┓                              
+┗┻┛┗ ┗┗┗┛┛┗┗┗    ┻ ┗┛                              
+┓ ┏•             ┓  ┏┓            ┳      ┓┓   •    
+┃┃┃┓┏┓┏┓┏┓┓┏┏┓┏┓┏┫  ┗┓┏┓┏┓┓┏┏┓┏┓  ┃┏┓┏╋┏┓┃┃┏┓╋┓┏┓┏┓
+┗┻┛┗┛ ┗ ┗┫┗┻┗┻┛ ┗┻  ┗┛┗ ┛ ┗┛┗ ┛   ┻┛┗┛┗┗┻┗┗┗┻┗┗┗┛┛┗
+         ┛                                         
+"
+
+if [ $(id -u) -ne 0 ]; then 
+    
+    echo "Permission Denied: Root Previlages Required"
+
+else
+
+    echo ""
+    echo "----------------------"
+    echo "Server Configurations"
+    echo "----------------------"
+    echo ""
+    echo "Installing WireGuard.../"
+    echo ""
+
+    if dpkg -l | grep -q "wireguard"; then
+
+        echo "-------------------------------"
+        echo "WireGuard is already installed."         
+        echo "-------------------------------"
+                
+    elif sudo apt install wireguard -y; then
+
+        echo "-----------------------------------"
+        echo "WireGuard is successfully installed"
+        echo "-----------------------------------"
+
+    fi
+
+    echo ""
+    echo "Adding NET FORWARDING for IPV4 AND IPC 6.../"
+    echo ""
+                
+    if ! grep -q "^net.ipv4.ip_forward=1" /etc/sysctl.conf; then
+
+        cat << EOF >> /etc/sysctl.conf
+net.ipv4.ip_forward=1
+EOF
+
+    else
+
+        echo "IPV4 Net Forwarding already exsists. Ignoring it"
+
+    fi
+
+    if ! grep -q "^net.ipv6.conf.all.forwarding=1" /etc/sysctl.conf; then
+
+    cat << EOF >> /etc/sysctl.conf
+net.ipv6.conf.all.forwarding=1
+EOF
+
+    else
+
+        echo "IPV6 Net Forwarding already exsists. Ignoring it"
+
+    fi
+
+    echo ""
+
+    sysctl -p
+
+    echo ""
+
+    cd /etc/wireguard/
+
+    if [ -f /etc/wireguard/wg0.conf ]; then
+
+        echo "Warning: wg0.conf already exists. Please inspect it before proceeding."
+
+    else
+
+        umask 077
+        wg genkey > privatekey
+        wg pubkey < privatekey > publickey
+
+        PRIVATE_KEY=$(<privatekey)
+        PUBLIC_KEY=$(<publickey)
+        DNS_SERVER="8.8.8.8, 8.8.4.4, 1.1.1.1"
+
+        while true; do
+        
+            read -p "Enter Network Interface Address with Subnet [default: 10.10.10.1/24]: " NET_INT_ADDR
+            NET_INT_ADDR=${NET_INT_ADDR:-10.10.10.1/24}
+
+            if [[ $NET_INT_ADDR =~ ^([0-9]{1,3}\.){3}[1-9][0-9]{0,2}/([1-9]|[12][0-9]|3[0-1])$ ]] && [[ ${NET_INT_ADDR##*/} -gt 8 && ${NET_INT_ADDR##*/} -lt 32 ]]; then
+
+                break
+
+            else
+
+                echo "Invalid IP address or subnet format. Please try again. Subnet must be greater than 8 and less than 32, and the fourth octet cannot be 0."
+
+            fi
+
+        done
+
+        while true; do
+
+            read -p "Enter Listening Port of the Server [default: 51820]: " LISTEN_PORT
+            LISTEN_PORT=${LISTEN_PORT:-51820}
+
+            if [[ $LISTEN_PORT =~ ^[0-9]+$ ]] && [ $LISTEN_PORT -ge 1 ] && [ $LISTEN_PORT -le 65535 ]; then
+
+                break
+
+            else
+
+                echo "Invalid port number. Please enter a number between 1 and 65535."
+
+            fi
+
+        done
+
+        # Automatically detect the network interface
+        INTERFACE=$(ip -o -4 route show to default | awk '{print $5}')
+
+        cat << EOF > wg0.conf
+[Interface]
+Address = $NET_INT_ADDR
+ListenPort = $LISTEN_PORT
+PrivateKey = $PRIVATE_KEY
+DNS = $DNS_SERVER
+
+PostUp = iptables -A FORWARD -i telecash-vpn -o $INTERFACE -j ACCEPT
+PostUp = iptables -t nat -A POSTROUTING -o $INTERFACE -j MASQUERADE
+PreDown = iptables -D FORWARD -i telecash-vpn -o $INTERFACE -j ACCEPT
+PreDown = iptables -t nat -D POSTROUTING -o $INTERFACE -j MASQUERADE
+EOF
+
+        echo ""
+        echo "wg0.conf has been created with the specified configurations. Server Configuration is now Successfully Complete."
+        echo ""
+        
+        echo "----------------------------------------------------------------"
+        echo "Server Configuration Details"
+        echo "----------------------------------------------------------------"
+        echo "PrivateKey = $PRIVATE_KEY"
+        echo "Address = $NET_INT_ADDR"
+        echo "Listening Port = $LISTEN_PORT"
+        echo "DNS Server = $DNS_SERVER"
+        echo "----------------------------------------------------------------"
+                
+    fi
+fi
